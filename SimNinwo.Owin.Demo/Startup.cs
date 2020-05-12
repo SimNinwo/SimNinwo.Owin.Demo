e6186fc2553b165ed5bf38b3c4b07839ecb6c54a
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-05-12 16:55:26 +0100

    Add project files.

---
+++ SimNinwo.Owin.Demo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace SimNinwo.Owin.Demo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app) // IAppBuilder interface used to add middleWares 
+        {
+            // middleware with katana
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("<html><head></head><body>Hello World</body></html>");
+            });
+        }
+    }
+}
